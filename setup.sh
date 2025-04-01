fa6d7538f83f1a75d3f27455b9de1d0a4ecab94e
blof33z <bvh88xz@proton.me>
2025-04-01 04:11:40 +0300

    Delete setup.sh

--- setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "[+] Gerekli paketler yükleniyor..."
-apt update && apt install -y tor proxychains curl macchanger openvpn network-manager net-tools firefox-esr
-echo "[✓] Kurulum tamamlandı."
